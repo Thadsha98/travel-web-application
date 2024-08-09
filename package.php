1333ec3f4516f91bbfff7a96d185ed74a422a92d
Thadsha98 <shajiniraj1998@gmail.com>
2024-08-09 21:24:46 +0530

    first commit

---
+++ package.php
@@ -0,0 +1,246 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>package</title>
+
+   <!-- swiper css link  -->
+   <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css" />
+
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+   
+<!-- header section starts  -->
+
+<section class="header">
+
+   <a href="home.php" class="logo">SRILANKAN TRAVEL.</a>
+
+   <nav class="navbar">
+      <a href="home.php">home</a>
+      <a href="about.php">about</a>
+      <a href="package.php">package</a>
+      <a href="book.php">book</a>
+   </nav>
+
+   <div id="menu-btn" class="fas fa-bars"></div>
+
+</section>
+
+<!-- header section ends -->
+
+<div class="heading" style="background:url(images/header-bg-2.png) no-repeat">
+   <h1>packages</h1>
+</div>
+
+<!-- packages section starts  -->
+
+<section class="packages">
+
+   <h1 class="heading-title">top destinations</h1>
+
+   <div class="box-container">
+
+      <div class="box">
+         <div class="image">
+            <img src=" images/sigieiya.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>sigiriya</h3>
+            <p>A visit to this place will show you an 1600 years old Sigiriya Fortress which was built by King Kasyapa. This massive compound is protected by two moats and three ramparts. It is once known to be the private residence of King Kasyapa and also one of the best places to visit in Sri Lanka.If you are interested in ancient places, this is one of the best tourist places to visit in Sri Lanka.</p>
+            <a href="book.php" class="btn">book now</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/toothrelic.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>Temple of the Sacred Tooth Relic</h3>
+            <p>Nestled close to the Udawattakele Sanctuary, The Temple of the Sacred Tooth Relic is one of the most important shrines of Sri Lanka. Also named as Dalada Maligawa, this temple is based in the royal palace of the Kingdom of Kandy.</p>
+            <a href="book.php" class="btn">book now</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/yala.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>yala national park</h3>
+            <p>Spotting a leopard prowling in the Park of Yala is quite an ordinary spectacle. Witness the untamed world of nature in the Yala National Park and quench your eagerness to explore and learn more about wildlife. Yala National Park is located in the South-East part of Sri Lanka</p>
+            <a href="book.php" class="btn">book now</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/buddhist.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>Dambulla Cave Temple</h3>
+            <p>Dambulla Cave temple is also known as the Golden Rock temple is also one of the world heritage city declared by UNESCO. The cave temple is made with a massive granite outcrop</p>
+            <a href="book.php" class="btn">book now</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/ambuliuvala.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>ambuluvala</h3>
+            <p>Ambuluwawa Tower is located in the suburbs of Gampola Town in Sri Lanka. There has been an increased popularity in recent times especially due to the breathtaking views you can experience when you climb to the top of the Ambuluwawa Tower. It is also known for its narrow spiral staircase which some visitors find challenging to climb. It is one of the coolest places to visit in Sri Lanka if you are up for the challenge. </p>
+            <a href="book.php" class="btn">book now</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/gallefort.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>Galle Fort Lighthouse</h3>
+            <p> Galle Lighthouse is a prominent tourist attraction of Galle. Situated on the shores of Galle, the lighthouse makes a postcard-perfect scenario against the emerald waves hitting the fine beige sands of Galle. Also, it is situated inside the architectural marvel of the city, Galle Fort, which defines its scenic beauty further.</p>
+            <a href="book.php" class="btn">book now</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/viharamadevi.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>Viharamahadevi Park</h3>
+            <p>Named after Queen Viharamahadevi, this park is the largest and probably the only public park & one of the best places to visit in Colombo. It is one of those places that never cease to excite you irrespective of the number of times you come here.</p>
+            <a href="book.php" class="btn">book now</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/zoo.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>Dehiwala Zoo</h3>
+            <p>Dehiwala Zoo or the National Zoological Gardens of Sri Lanka was set up during 1936 with the sole ambition of providing ultimate protection to a wide range of wild animals and birds.</p>
+            <a href="book.php" class="btn">book now</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/bridge.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>nine arch bridge</h3>
+            <p>The Nine Arch Bridge, also known as the ‘Bridge in the Sky’ was constructed by connecting two bog mountains when constructing the Badulla – Colombo railway. This bridge is 300 feet in length, 25 feet in width and 80-100 feet in height. It is one of the best examples of colonial-era railway construction in the country.</p>
+            <a href="book.php" class="btn">book now</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/coconut1.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>mirissa hill</h3>
+            <p>Fringing two sandy bays separated by a rock island reachable at low tide, Mirissa feels hidden away compared to Sri Lanka’s other busier resort towns. While the quiet sands are sprinkled with bars and surf rentals, it’s Mirissa’s May-to-November whale-watching boat trips that steal the show.</p>
+            <a href="book.php" class="btn">book now</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/download.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>nallur temple</h3>
+            <p>This place is also called the city of temples in Sri Lanka, as it has some of the most beautiful temples of Sri Lanka and one of the best Sri lanka attractions for spiritual retreat. The most visited temples of all in this place are the temple of Nallur Kovil.</p>
+            <a href="book.php" class="btn">book now</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/sinharaja.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>singaraja forest</h3>
+            <p> the most beautiful places in Sri Lanka is incomplete without mentioning Sinharaja Forest Reserve. If there is one thing in abundance in Sri lanka, it is flora-fauna. The home of lions, Sinharaja Forest Reserve is a paradise for wildlife enthusiasts and nature lovers making it one of the top places to visit in Sri lanka. The reason why you should visit this place is that you will find a variety of endemic birds and mammals.</p>
+            <a href="book.php" class="btn">book now</a>
+         </div>
+      </div>
+
+   </div>
+
+   <div class="load-more"><span class="btn">load more</span></div>
+
+</section>
+
+<!-- packages section ends -->
+
+
+<!-- footer section starts  -->
+
+<section class="footer">
+
+   <div class="box-container">
+
+      <div class="box">
+         <h3>quick links</h3>
+         <a href="home.php"> <i class="fas fa-angle-right"></i> home</a>
+         <a href="about.php"> <i class="fas fa-angle-right"></i> about</a>
+         <a href="package.php"> <i class="fas fa-angle-right"></i> package</a>
+         <a href="book.php"> <i class="fas fa-angle-right"></i> book</a>
+      </div>
+
+      <div class="box">
+         <h3>extra links</h3>
+         <a href="#"> <i class="fas fa-angle-right"></i> ask questions</a>
+         <a href="#"> <i class="fas fa-angle-right"></i> about us</a>
+         <a href="#"> <i class="fas fa-angle-right"></i> privacy policy</a>
+         <a href="#"> <i class="fas fa-angle-right"></i> terms of use</a>
+      </div>
+
+      <div class="box">
+         <h3>contact info</h3>
+         <a href="#"> <i class="fas fa-phone"></i>  021-2270114 </a>
+         <a href="#"> <i class="fas fa-phone"></i>021-2221114  </a>
+         <a href="#"> <i class="fas fa-envelope"></i>shajiraj1998@gmail.com </a>
+         <a href="#"> <i class="fas fa-map"></i>wellawatte,colombo. </a>
+      </div>
+
+      <div class="box">
+         <h3>follow us</h3>
+         <a href="#"> <i class="fab fa-facebook-f"></i> facebook </a>
+         <a href="#"> <i class="fab fa-twitter"></i> twitter </a>
+         <a href="#"> <i class="fab fa-instagram"></i> instagram </a>
+         <a href="#"> <i class="fab fa-linkedin"></i> linkedin </a>
+      </div>
+
+   </div>
+
+   <div class="credit"> created by <span>THADSHAJINI</span> | all rights reserved! </div>
+
+</section>
+
+<!-- footer section ends -->
+
+<!-- swiper js link  -->
+<script src="https://unpkg.com/swiper@7/swiper-bundle.min.js"></script>
+
+<!-- custom js file link  -->
+<script src="js/script.js"></script>
+
+</body>
+</html>
